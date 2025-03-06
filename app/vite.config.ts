8a3053c8043a32972ba88b30da6c554a0626f0e0
kientt265 <kientran26503@gmail.com>
2025-03-06 13:12:53 +0700

    add_front_end

---
+++ app/vite.config.ts
@@ -0,0 +1,16 @@
+// WSL USERS ON WINDOWS ONLY (NOT NECESSARY FOR LINUX/MACOS)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    tailwindcss(),
+  ],
+  
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
